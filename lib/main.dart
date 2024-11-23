53c912516202abb138d58a99b9bacf37c9141849
Rizwanullah10 <113945217+Rizwanullah10@users.noreply.github.com>
2024-11-23 19:35:22 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Name Display App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Center(
+          child: Text(
+            'RIZWAN ULLAH',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
